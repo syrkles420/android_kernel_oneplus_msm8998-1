e76bccbc4ea3d1dab58064fcc267c8eaa048bcdf
Francisco Franco <franciscofranco.1990@gmail.com>
2018-12-29 06:00:13 +0000

    crypto: lz4: fix lz4_compress_crypto
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- crypto/lz4.c
+++ crypto/lz4.c
@@ -50,7 +50,7 @@ static int lz4_compress_crypto(struct crypto_tfm *tfm, const u8 *src,
 {
 	struct lz4_ctx *ctx = crypto_tfm_ctx(tfm);
 	int out_len = LZ4_compress_default(src, dst,
-		slen, *dlen, ctx->lz4_comp_mem);
+		slen, *dlen, crypto_tfm_ctx(tfm));
 
 	if (!out_len)
 		return -EINVAL;
